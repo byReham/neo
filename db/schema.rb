fb874da422395f21dbd7a27a5877436f7fb2ec84
byReham <byreham@gmail.com>
2016-12-20 22:50:37 +0300

    Imlement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210123536) do
+ActiveRecord::Schema.define(version: 20161220184704) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20161210123536) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
